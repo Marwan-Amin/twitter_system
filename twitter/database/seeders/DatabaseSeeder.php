6cd8dc9cd0bd7ec41fef1bb2840f50e1e1bad8ec
Marwan-Amin <marwanamin09@gmail.com>
2021-04-25 23:55:20 +0200

    Removed model factories seeding

--- twitter/database/seeders/DatabaseSeeder.php
+++ twitter/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(1000)->create();
-        \App\Models\Tweet::factory(4000)->create();
+        // \App\Models\User::factory(1000)->create();
+        // \App\Models\Tweet::factory(4000)->create();
     }
 }
